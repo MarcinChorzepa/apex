e0899304e2b20b54a3f7e4842ef0c7edf27ea396
MarcinChorzepa <mchorzepa1@tlen.pl>
2018-01-28 20:15:27 +0100

    fulsh shared pool

--- EXPLAIN PLAN/select_explain_plan_oracle.sql
+++ EXPLAIN PLAN/select_explain_plan_oracle.sql
@@ -1,4 +1,12 @@
+
+/*set the name of query plan*/
 EXPLAIN PLAN SET STATEMENT_ID ='nazwa' FOR
 SELECT * FROM aws.dane ;
+
+/*select the plan for the name*/
 SELECT *
 FROM TABLE (DBMS_XPLAN.DISPLAY(NULL, 'nazwa', 'ALL'));
+
+/*clear all plans from cache*/
+alter system flush shared_pool;
+
