2a72e2e0ff47c1f60dde58f08e2022873b1019b8
MarcinChorzepa <mchorzepa1@tlen.pl>
2018-02-06 09:11:06 +0100

    Package with static

---
+++ PL_SQL_PACKAGE/create package and body.sql
@@ -0,0 +1,17 @@
+/*creujemy pakiet ze stałą
+oraz funkcję do użycia w select*/
+create or replace package tmp as
+    kpi_7 number := 80;
+    FUNCTION kpi_7 RETURN NUMBER ;
+end ;
+/
+/*packaga body*/
+create or replace PACKAGE BODY tmp AS
+    FUNCTION kpi_7 RETURN NUMBER IS
+      BEGIN
+      return tmp.kpi_7;
+    end kpi_7;
+end;
+/
+/*how to use the package function*/
+SELECT tmp.kpi_7() FROM dual;
